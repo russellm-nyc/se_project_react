649ba9fd38fd88f2eb7f25d6666c8a3212f12447
russellm <russell@russellm.com>
2025-08-31 17:25:34 -0400

    WTWR phase 1-5

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+import "../blocks/App.css";
+function App() {
+  return (
+    <div className="app">
+      {" "}
+      <Header /> <Main /> <Footer />{" "}
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import logo from "../assets/logo.svg";
+import avatar from "../assets/avatar.svg";
+import "../blocks/Header.css";
+
+function Header() {
+  const now = new Date();
+  const dateStr = now.toLocaleDateString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <img src={logo} alt="WTWR Logo" className="header__logo" />
+      <p className="header__place">
+        <time className="header__datetime" dateTime="now">
+          {dateStr}
+        </time>
+        , New York
+      </p>
+      <p className="header__username">
+        <button className="header__add-clothes-btn">+ Add clothes</button>
+        Terrence Tegegne
+      </p>
+      <img
+        src={avatar}
+        alt="Terrence Tegegne's avatar"
+        className="header__avatar"
+      />
+    </header>
+  );
+}
+
+export default Header;
