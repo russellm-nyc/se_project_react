f8743d0866c33da885102eec1c5240db6c5ff9d1
russellm <russell@russellm.com>
2025-09-21 04:18:55 -0400

    created a reusable function in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,28 @@
 const baseUrl = "http://localhost:3001";
-function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => {
+
+function request(url, options = {}) {
+  return fetch(url, options).then((res) => {
     return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
 }
 
+function getItems() {
+  return request(`${baseUrl}/items`);
+}
+
 function addItem({ name, imageUrl, weather }) {
-  return fetch(`${baseUrl}/items`, {
+  return request(`${baseUrl}/items`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, imageUrl, weather }),
-  }).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
 }
 
 function deleteItem(id) {
-  return fetch(`${baseUrl}/items/${id}`, {
+  return request(`${baseUrl}/items/${id}`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
-  }).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
 }
+
 export { getItems, addItem, deleteItem };
